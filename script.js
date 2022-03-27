af467ec4dc8a4fdd638008006c9a831d7f69a0ed
Ankit <ankit.95sharma@gmail.com>
2022-03-27 17:59:17 +0530

    highlight logic corrected

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ let score = 1;
 const patternArr = [];
 const userArr = [];
 
-const scoreElement = document.querySelector('.score_container');
+const scoreElement = document.querySelector(".score_container");
+console.log(document.getElementById("n2"));
 
-scoreElement.addEventListener('click', () => {
+scoreElement.addEventListener("click", () => {
   scoreElement.textContent = score;
   populatePattern();
 });
@@ -21,22 +22,38 @@ const generateRand = () => {
   return randNum;
 };
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const syncWait = (ms) => {
-  const end = Date.now() + ms;
-  while (Date.now() < end) continue;
-};
+// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// const syncWait = (ms) => {
+//   const end = Date.now() + ms;
+//   while (Date.now() < end) continue;
+// };
 
 const changeOpacity = (ele, opacityValue) => {
   document.querySelector(`#n${ele}`).style.opacity = opacityValue;
 };
 
 const highLightDiv = (list) => {
-  list.forEach((ele) => {
-    // console.log(document.querySelector(`#n${ele}`));
-    changeOpacity(ele, 1);
-    sleep(1000);
-    changeOpacity(ele, 0.3);
+  list.forEach((ele, index) => {
+    setTimeout(() => {
+      document.querySelector(`#n${ele}`).style.opacity = 1;
+    }, 700 * index);
+
+    setTimeout(() => {
+      document.querySelectorAll(".tiles").forEach((ele) => {
+        ele.style.opacity = 0.3;
+      });
+    }, 700 * (index + 0.5));
+
+    // setTimeout(() => {
+    //   document.querySelector(`#n${ele}`).style.opacity = 0.3;
+    // }, 500 * (index + 1));
   });
-  //Todo: add sound effect
 };
+
+// list.forEach((ele) => {
+//   // console.log(document.querySelector(`#n${ele}`));
+//   changeOpacity(ele, 1);
+//   setTimeout(() => changeOpacity(ele, 0.3), 1000);
+//   // changeOpacity(ele, 0.3);
+// });
+//Todo: add sound effect
