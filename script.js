75e90aeaac41b870c019fba6643d8cf3adc9b0e6
codemdshahbazali <codemdshahbazali@gmail.com>
2022-03-27 23:07:42 +0530

    improved logic

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const patternArr = [];
 const userArr = [];
 
 const scoreElement = document.querySelector('.score_container');
+// const tile1 = document.querySelector('.tile1');
+// const tile2 = document.querySelector('.tile2');
+// const tile3 = document.querySelector('.tile3');
+// const tile4 = document.querySelector('.tile4');
+
+const tiles = document.querySelectorAll('.tiles');
 
 scoreElement.addEventListener('click', () => {
   scoreElement.textContent = score;
@@ -11,7 +17,7 @@ scoreElement.addEventListener('click', () => {
 
 const populatePattern = () => {
   patternArr.push(generateRand());
-  highLightDiv(patternArr);
+  setTimeout(() => highLightDiv(patternArr), 500);
 };
 
 const generateRand = () => {
@@ -21,22 +27,57 @@ const generateRand = () => {
   return randNum;
 };
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-const syncWait = (ms) => {
-  const end = Date.now() + ms;
-  while (Date.now() < end) continue;
-};
-
-const changeOpacity = (ele, opacityValue) => {
-  document.querySelector(`#n${ele}`).style.opacity = opacityValue;
-};
+const changeOpacity = (ele, opacityValue) =>
+  (document.querySelector(`#n${ele}`).style.opacity = opacityValue);
 
 const highLightDiv = (list) => {
-  list.forEach((ele) => {
-    // console.log(document.querySelector(`#n${ele}`));
-    changeOpacity(ele, 1);
-    sleep(1000);
-    changeOpacity(ele, 0.3);
+  //   list.forEach(async (ele) => {
+  //     // console.log(document.querySelector(`#n${ele}`));
+  //     changeOpacity(ele, 1);
+  //     changeOpacity(ele, 0.3);
+  //     //Todo: add sound effect
+  //   });
+  let lengthOfList = list.length;
+  let count = 0;
+
+  let interval = setInterval(() => {
+    let currentCount = count;
+    changeOpacity(list[currentCount], 1);
+    setTimeout(() => changeOpacity(list[currentCount], 0.3), 300);
+    count++;
+    if (count === lengthOfList) clearInterval(interval);
+  }, 800);
+};
+
+console.log(tiles);
+
+tiles.forEach((ele) => {
+  ele.addEventListener('click', (event) => {
+    console.log(event.target.id[1]);
+    let idNumber = Number(event.target.id[1]);
+    userArr.push(idNumber);
+    changeOpacity(idNumber, 1);
+    setTimeout(() => changeOpacity(idNumber, 0.3), 300);
+    if (!compareInputs(patternArr, userArr)) {
+      highLightDiv(patternArr);
+    } else {
+      if (patternArr.length === userArr.length) {
+        console.log('here');
+        score++;
+        scoreElement.textContent = score;
+        populatePattern();
+      }
+    }
   });
-  //Todo: add sound effect
+});
+
+const compareInputs = (patternArr, userArr) => {
+  if (userArr.length === 0 || patternArr.length === 0) return false;
+  for (let i = 0; i < userArr.length; i++) {
+    if (patternArr[i] !== userArr[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
